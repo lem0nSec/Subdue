a222fbc9e630200e51d3114598f1142b405eafb9
lem0nSec <98479572+lem0nSec@users.noreply.github.com>
2022-02-01 18:30:37 +0100

    Update Subdue.py

--- Subdue.py
+++ Subdue.py
@@ -3,14 +3,27 @@
 # Author: Lem0nSec_
 # Version: 1.0 
 
-# This tool will fuzz subdomains within a specified domain and using a specified wordlist. 
-# A positive result is given when the values of 'content bytes' and 'content lines' for a
-# fuzzed potential subdomain diverge from those of another subdomain obtained using a 
-# random string.
-# Bear in mind that Subdue has been written with CTF-like challenges in mind. For this reason, 
-# it may not be a suitable tool for real-world engagements.
-
-# All rights are reserved
+# MIT License
+#
+# Copyright (c) 2022 lem0nSec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import argparse
 import string
